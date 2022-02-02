4238b09ea7e40122aafe8e9ce80f43a2284ae413
Chriszo <ChristoZort@gmail.com>
2022-02-03 01:06:00 +0200

    02-02-22-3: Added SearchFilters Builder and listeners to Framelayout view components.

--- app/src/main/java/com/example/hikingapp/persistence/RouteInfo.kt
+++ app/src/main/java/com/example/hikingapp/persistence/RouteInfo.kt
@@ -1,19 +1,15 @@
 package com.example.hikingapp.persistence
 
+import android.media.Rating
 import com.example.hikingapp.domain.DifficultyLevel
 import com.example.hikingapp.domain.RouteType
 
-class RouteInfo {
+class RouteInfo(
+    var distance: Int,
+    var timeEstimation: Double,
+    var routeType: RouteType,
+    var difficultyLevel: DifficultyLevel,
+    var rating: Float
+) {
 
-    private var distance: Int
-    private var timeEstimation: Double
-    private var routeType: RouteType
-    private var difficultyLevel: DifficultyLevel
-
-    constructor(distance: Int, timeEstimation: Double, routeType: RouteType, difficultyLevel: DifficultyLevel) {
-        this.distance = distance
-        this.timeEstimation = timeEstimation
-        this.routeType = routeType
-        this.difficultyLevel = difficultyLevel
-    }
 }
--- app/src/main/java/com/example/hikingapp/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/example/hikingapp/ui/discover/DiscoverFragment.kt
@@ -8,22 +8,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import android.widget.TextView
-import androidx.activity.addCallback
 import androidx.annotation.RequiresApi
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
-import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.hikingapp.R
 import com.example.hikingapp.databinding.FragmentDiscoverBinding
+import com.example.hikingapp.domain.DifficultyLevel
+import com.example.hikingapp.domain.RouteType
+import com.example.hikingapp.search.SearchFiltersWrapper
 import com.example.hikingapp.search.SearchType
 import com.example.hikingapp.search.SearchUtils
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.geojson.Point
-import com.mapbox.navigation.ui.utils.internal.extensions.slideHeight
 import com.mapbox.search.*
 import com.mapbox.search.result.SearchResult
 import com.mapbox.search.result.SearchSuggestion
@@ -36,12 +33,15 @@ class DiscoverFragment : Fragment() {
     private lateinit var discoverViewModel: DiscoverViewModel
     private var _binding: FragmentDiscoverBinding? = null
 
+
     // This property is only valid between onCreateView and
     // onDestroyView.
 //    private val binding get() = _binding!!
 
     private lateinit var searchEngine: SearchEngine
     private lateinit var searchRequestTask: SearchRequestTask
+
+    private lateinit var searchFiltersWrapperBuilder: SearchFiltersWrapper.Builder
 
     private val searchCallback = object : SearchSelectionCallback {
 
@@ -140,6 +140,8 @@ class DiscoverFragment : Fragment() {
         root.btn_filters_ok.setOnClickListener {
             BottomSheetBehavior.from(_binding!!.filterSheet).apply {
                 this.state = BottomSheetBehavior.STATE_COLLAPSED
+                val searchFilters = searchFiltersWrapperBuilder.build()
+                println(searchFilters)
             }
         }
 
@@ -147,9 +149,39 @@ class DiscoverFragment : Fragment() {
 
             BottomSheetBehavior.from(_binding!!.filterSheet).apply {
                 this.state = BottomSheetBehavior.STATE_EXPANDED
+                searchFiltersWrapperBuilder = SearchFiltersWrapper.Builder()
             }
         }
 
+        root.btn_linear.setOnClickListener {
+
+            searchFiltersWrapperBuilder.withType(RouteType.LINEAR)
+        }
+
+        root.btn_cyclic.setOnClickListener {
+            searchFiltersWrapperBuilder.withType(RouteType.CYCLIC)
+        }
+
+        root.btn_easy.setOnClickListener {
+            searchFiltersWrapperBuilder.withDifficulty(DifficultyLevel.EASY)
+        }
+
+        root.btn_moderate.setOnClickListener {
+            searchFiltersWrapperBuilder.withDifficulty(DifficultyLevel.MODERATE)
+        }
+
+        root.btn_hard.setOnClickListener {
+            searchFiltersWrapperBuilder.withDifficulty(DifficultyLevel.HARD)
+        }
+
+        root.ratingBar.setOnRatingBarChangeListener { _, rating, _ ->
+            searchFiltersWrapperBuilder.withRating(rating)
+        }
+
+        root.distance_slider.addOnChangeListener { slider, value, fromUser ->
+            println(value)
+            searchFiltersWrapperBuilder.withDistance(value.toDouble())
+        }
 
         root.search_bar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 
