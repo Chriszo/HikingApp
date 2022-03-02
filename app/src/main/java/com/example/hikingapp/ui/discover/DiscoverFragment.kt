a21ecfe85b74379fc0f416692e4ab35a769b1ce1
Chriszo <ChristoZort@gmail.com>
2022-03-02 08:24:10 +0200

    02-03-22-02: Use of current user's location in search functionalities.

--- app/src/main/java/com/example/hikingapp/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/example/hikingapp/ui/discover/DiscoverFragment.kt
@@ -1,7 +1,12 @@
 package com.example.hikingapp.ui.discover
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +15,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -42,15 +49,14 @@ import java.util.stream.Collectors
 import kotlin.concurrent.schedule
 
 
-class DiscoverFragment : Fragment(), OnItemClickedListener {
+class DiscoverFragment : Fragment(), OnItemClickedListener, LocationListener {
 
     private lateinit var discoverViewModel: DiscoverViewModel
     private var _binding: FragmentDiscoverBinding? = null
 
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-//    private val binding get() = _binding!!
+    private val locationManager by lazy {
+        activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
 
     private lateinit var searchEngine: SearchEngine
     private lateinit var searchRequestTask: SearchRequestTask
@@ -123,6 +129,25 @@ class DiscoverFragment : Fragment(), OnItemClickedListener {
 
         _binding = FragmentDiscoverBinding.inflate(inflater, container, false)
 
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return _binding!!.root
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, this)
+
         BottomSheetBehavior.from(_binding!!.filterSheet).apply {
             this.peekHeight = 0
             this.state = BottomSheetBehavior.STATE_COLLAPSED
@@ -152,12 +177,6 @@ class DiscoverFragment : Fragment(), OnItemClickedListener {
         val root: View = _binding!!.root
 
         val searchType = SearchType.BY_PLACE
-
-        userLocation = Point.fromLngLat(
-            21.942563928989884,
-            39.23945243147539  // TODO Replace with current location of user
-        )
-
 
         MapboxSearchSdk.initialize(
             application = requireActivity().application,
@@ -198,7 +217,7 @@ class DiscoverFragment : Fragment(), OnItemClickedListener {
                                 routeSearchResults = SearchUtils.performGeocodingAPICall(
                                     userLocation,
                                     keyword
-                                ) // TODO Change with user's lccation
+                                )
                             }
                         }
                     } else {
@@ -309,5 +328,9 @@ class DiscoverFragment : Fragment(), OnItemClickedListener {
         val intent = Intent(context, RouteActivity::class.java)
         intent.putExtra("route", currentRoutes[position])
         startActivity(intent)
+    }
+
+    override fun onLocationChanged(location: Location) {
+        userLocation = Point.fromLngLat(location.longitude,location.latitude)
     }
 }
